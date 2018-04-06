3b6da6f479b8a35d0b7ee485c9b82e3e22af729b
Trillianjose <trillianjosea@gmail.com>
2018-04-05 21:13:40 -0500

    Test add

--- Gemfile
+++ Gemfile
@@ -40,7 +40,7 @@ group :development, :test do
   gem 'capybara', '~> 2.13'
   gem 'selenium-webdriver'
   gem 'factory_bot_rails'
-  gem 'faker', :git => 'https://github.com/stympy/faker.git', :branch => 'master'
+  gem 'faker'
 end
 
 group :development do
--- spec/models/expense_spec.rb
+++ spec/models/expense_spec.rb
@@ -4,8 +4,29 @@ RSpec.describe Expense, type: :model do
   context "The expense exists" do
     it "should belong to a user" do
       time = Time.now.freeze
-      expense = Expense.new(amount: 30000, concept: "Uber", date: time)
-      expect(expenses.valid?).to eq(true)
+      user = User.create(email: Faker::Internet.email, password: Faker::Internet.password(6))
+      expense = Expense.new(amount: 30000, concept: "Uber", date: time, user: user)
+      expect(expense.valid?).to eq(true)
+      expect(expense.save).to eq(true)
+      expect(Expense.count).to eq(1)
+      expect(user.expenses).to eq([expense])
+    end
+    context "amount related" do
+      before do
+        time = Time.now.freeze
+        user = User.create(email: Faker::Internet.email, password: Faker::Internet.password(6))
+        @expense = Expense.new(amount: 30000, concept: "Uber", date: time, user: user)
+      end
+    end
+    context "the amount is negative" do
+      it "should not save the expense" do
+        expect(@expense.valid?).to be_falsey
+        expect(@expense.save).to be_falsey
+      end
+    end
+    context "the amount is positive" do
+      it "should save the expense" do
+      end
     end
   end
 end
