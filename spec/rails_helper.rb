9dd0b7282c3c5123232dcb34336cbba5866aba1a
Trillianjose <trillianjosea@gmail.com>
2018-04-10 20:16:36 -0500

    Added Dashboard controller

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,12 +1,14 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 require 'faker'
-require "capybara/rspec"
+
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
+require "capybara/rspec"
 require 'rspec/rails'
+require "capybara"
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -77,6 +79,7 @@ RSpec.configure do |config|
   config.after(:each) do
     DatabaseCleaner.clean
   end
+  config.include Capybara::DSL
 end
 
 #RSpec.describe "Signing in", js: true do
