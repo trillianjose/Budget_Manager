cadffe947522ce9d0699dd0dd29cc22b64be7309
Trillianjose <trillianjosea@gmail.com>
2018-05-03 21:03:01 -0500

    Using filters

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,10 @@
 class DashboardController < ApplicationController
 
   def index
+    @today_expenses = DashboardHandler.today_expenses
+    @yesterday_expenses = DashboardHandler.yesterday_expenses
+    @this_month_expenses = DashboardHandler.this_month_expenses
+    @last_month_expenses = DashboardHandler.last_month_expenses
   end
-  
+
 end
--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -3,5 +3,10 @@ class ExpensesController < ApplicationController
     @expenses = Expense.all
     @transaction_type = TransactionType.all
     @category = Category.all
+
+    respond_to do |format|
+      format.html { render :index }
+      format.json { render json: @expenses, status: :ok }
+    end
   end
 end
